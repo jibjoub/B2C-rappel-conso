93c1d2cc4e3641faecf31bee4cceb6553e1bd517
SVaury <sophie.vaury@cb-plus.eu>
2021-12-22 14:05:51 +0100

    add product ListView

--- RappelConso/app/src/main/java/view/ui/MainActivity.kt
+++ RappelConso/app/src/main/java/view/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.rappelconso
+package view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rappelconso.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
